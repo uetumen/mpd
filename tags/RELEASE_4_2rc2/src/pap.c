272345f2f503aec55d56fe2f327928df0eed73ee
nobody <nobody@d3cc347e-4bbe-e711-9839-001b216d295b>
2007-06-23 12:08:43 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_4_2rc2'.

---
+++ tags/RELEASE_4_2rc2/src/pap.c
@@ -0,0 +1,307 @@
+
+/*
+ * pap.c
+ *
+ * Written by Toshiharu OHNO <tony-o@iij.ad.jp>
+ * Copyright (c) 1993, Internet Initiative Japan, Inc. All rights reserved.
+ * See ``COPYRIGHT.iij''
+ * 
+ * Rewritten by Archie Cobbs <archie@freebsd.org>
+ * Copyright (c) 1995-1999 Whistle Communications, Inc. All rights reserved.
+ * See ``COPYRIGHT.whistle''
+ */
+
+#include "ppp.h"
+#include "auth.h"
+#include "util.h"
+
+/*
+ * INTERNAL FUNCTIONS
+ */
+
+  static void	PapSendRequest(Link l);
+  static void	PapTimeout(void *ptr);
+
+/*
+ * PapStart()
+ */
+
+void
+PapStart(Link l, int which)
+{
+  PapInfo pap = &l->lcp.auth.pap;
+
+  switch (which) {
+    case AUTH_PEER_TO_SELF:	/* Just wait for peer's request */
+      break;
+
+    case AUTH_SELF_TO_PEER:
+
+      /* Initialize retry counter and timer */
+      pap->next_id = 1;
+      pap->retry = AUTH_RETRIES;
+
+      TimerInit(&pap->timer, "PapTimer",
+	l->conf.retry_timeout * SECONDS, PapTimeout, l);
+      TimerStart(&pap->timer);
+
+      /* Send first request */
+      PapSendRequest(l);
+      break;
+
+    default:
+      assert(0);
+  }
+}
+
+/*
+ * PapStop()
+ */
+
+void
+PapStop(PapInfo pap)
+{
+  TimerStop(&pap->timer);
+}
+
+/*
+ * PapSendRequest()
+ *
+ * Send a PAP packet to peer.
+ */
+
+static void
+PapSendRequest(Link l)
+{
+    PapInfo		pap = &l->lcp.auth.pap;
+    char		password[AUTH_MAX_PASSWORD];
+    int			name_len, pass_len;
+    u_char		*pkt;
+
+    /* Get password corresponding to my authname */
+    Log(LG_AUTH, ("[%s] PAP: using authname \"%s\"", 
+	l->name, l->lcp.auth.conf.authname));
+    if (l->lcp.auth.conf.password[0] != 0) {
+	strncpy(password, l->lcp.auth.conf.password, sizeof(password));
+    } else if (AuthGetData(l->lcp.auth.conf.authname, password, 
+	    sizeof(password), NULL, NULL) < 0) {
+	Log(LG_AUTH, (" Warning: no secret for \"%s\" found", 
+	    l->lcp.auth.conf.authname));
+    }
+
+    /* Build response packet */
+    name_len = strlen(l->lcp.auth.conf.authname);
+    pass_len = strlen(password);
+
+    pkt = Malloc(MB_AUTH, 1 + name_len + 1 + pass_len);
+    pkt[0] = name_len;
+    memcpy(pkt + 1, l->lcp.auth.conf.authname, name_len);
+    pkt[1 + name_len] = pass_len;
+    memcpy(pkt + 1 + name_len + 1, password, pass_len);
+
+    /* Send it off */
+    AuthOutput(l, PROTO_PAP, PAP_REQUEST, pap->next_id++, pkt,
+	1 + name_len + 1 + pass_len, 0, 0);
+    Freee(MB_AUTH, pkt);
+}
+
+/*
+ * PapInput()
+ *
+ * Accept an incoming PAP packet
+ */
+
+void
+PapInput(Link l, AuthData auth, const u_char *pkt, u_short len)
+{
+  Auth			const a = &l->lcp.auth;
+  PapInfo		const pap = &a->pap;
+  PapParams		const pp = &auth->params.pap;
+  char			buf[32];
+
+  /* Deal with packet */
+  Log(LG_AUTH, ("[%s] PAP: rec'd %s #%d",
+    l->name, PapCode(auth->code, buf, sizeof(buf)), auth->id));
+  switch (auth->code) {
+    case PAP_REQUEST:
+      {
+	char		*name_ptr, name[256];
+	char		*pass_ptr, pass[256];
+	int		name_len, pass_len;
+	char		buf[32];
+
+	/* Is this appropriate? */
+	if (a->peer_to_self != PROTO_PAP) {
+	  Log(LG_AUTH, ("[%s] PAP: %s not expected",
+	    l->name, PapCode(auth->code, buf, sizeof(buf))));
+	  auth->why_fail = AUTH_FAIL_NOT_EXPECTED;
+	  PapInputFinish(l, auth);
+	  break;
+	}
+
+	name_len = pkt[0];
+	name_ptr = (char *)pkt + 1;
+
+	/* Sanity check packet and extract fields */
+	if (1 + name_len >= len
+	  || ((pass_len = pkt[1 + name_len]) && FALSE)
+	  || ((pass_ptr = (char *)pkt + 1 + name_len + 1) && FALSE)
+	  || name_len + 1 + pass_len + 1 > len)
+	{
+	  Log(LG_AUTH, (" Bad packet"));
+	  auth->why_fail = AUTH_FAIL_INVALID_PACKET;
+	  PapInputFinish(l, auth);
+	  break;
+	}
+	memcpy(name, name_ptr, name_len);
+	name[name_len] = 0;
+	memcpy(pass, pass_ptr, pass_len);
+	pass[pass_len] = 0;
+
+	strlcpy(pp->peer_name, name, sizeof(pp->peer_name));
+	strlcpy(pp->peer_pass, pass, sizeof(pp->peer_pass));
+	strlcpy(auth->params.authname, name, sizeof(auth->params.authname));
+	auth->params.password[0] = 0;
+
+	auth->finish = PapInputFinish;
+	AuthAsyncStart(l, auth);
+
+      }
+      break;
+
+    case PAP_ACK:
+    case PAP_NAK:
+      {
+	char	*msg;
+	int	msg_len;
+	char	buf[32];
+
+	/* Is this appropriate? */
+	if (a->self_to_peer != PROTO_PAP) {
+	  Log(LG_AUTH, ("[%s] PAP: %s not expected",
+	    l->name, PapCode(auth->code, buf, sizeof(buf))));
+	  break;
+	}
+
+	/* Stop resend timer */
+	TimerStop(&pap->timer);
+
+	/* Show reply message */
+	msg_len = pkt[0];
+	msg = (char *) &pkt[1];
+	if (msg_len < len - 1)
+	  msg_len = len - 1;
+	ShowMesg(LG_AUTH, msg, msg_len);
+
+	/* Done with my auth to peer */
+	AuthFinish(l, AUTH_SELF_TO_PEER, auth->code == PAP_ACK);	
+	AuthDataDestroy(auth);
+      }
+      break;
+
+    default:
+      Log(LG_AUTH, ("[%s] PAP: unknown code", l->name));
+      AuthDataDestroy(auth);
+      break;
+  }
+}
+
+/*
+ * PapInputFinish()
+ *
+ * Possible return point from the asynch auth handler.
+ * 
+ */
+ 
+void PapInputFinish(Link l, AuthData auth)
+{
+  Auth		const a = &l->lcp.auth;
+  PapParams	pap = &a->params.pap;
+  const char	*Mesg;
+  
+  Log(LG_AUTH, ("[%s] PAP: PapInputFinish: status %s", 
+    l->name, AuthStatusText(auth->status)));
+
+  if (auth->status == AUTH_STATUS_FAIL)
+    goto badRequest;
+  else if (auth->status == AUTH_STATUS_SUCCESS)
+    goto goodRequest;
+  
+  /* Do name & password match? */
+  if (strcmp(a->params.authname, pap->peer_name) ||
+      strcmp(a->params.password, pap->peer_pass)) {
+    Log(LG_AUTH, (" Invalid response"));
+    auth->why_fail = AUTH_FAIL_INVALID_LOGIN;
+    goto badRequest;
+  }
+  
+  goto goodRequest;
+
+badRequest:
+  {
+    char        failMesg[64];
+
+    Mesg = AuthFailMsg(auth, 0, failMesg, sizeof(failMesg));
+    Log(LG_AUTH, (" Reply message: %s", Mesg));
+    AuthOutput(l, PROTO_PAP, PAP_NAK, auth->id, (u_char *) Mesg, strlen(Mesg), 1, 0);
+    AuthFinish(l, AUTH_PEER_TO_SELF, FALSE);
+    AuthDataDestroy(auth);  
+    return;
+  }
+  
+goodRequest:
+  /* Login accepted */
+  Log(LG_AUTH, (" Response is valid"));
+  if (auth->reply_message) {
+    Mesg = auth->reply_message;
+  } else {
+    Mesg = AUTH_MSG_WELCOME;
+  }
+  Log(LG_AUTH, (" Reply message: %s", Mesg));
+  AuthOutput(l, PROTO_PAP, PAP_ACK, auth->id, (u_char *) Mesg, strlen(Mesg), 1, 0);
+  AuthFinish(l, AUTH_PEER_TO_SELF, TRUE);  
+  AuthDataDestroy(auth);
+}
+
+/*
+ * PapTimeout()
+ *
+ * Timer expired for reply to our request
+ */
+
+static void
+PapTimeout(void *ptr)
+{
+  Link		const l = (Link) ptr;
+  PapInfo	const pap = &l->lcp.auth.pap;
+
+  TimerStop(&pap->timer);
+  if (--pap->retry > 0) {
+    TimerStart(&pap->timer);
+    PapSendRequest(l);
+  }
+}
+
+/*
+ * PapCode()
+ */
+
+const char *
+PapCode(int code, char *buf, size_t len)
+{
+  switch (code) {
+    case PAP_REQUEST:
+      strlcpy(buf, "REQUEST", len);
+      break;
+    case PAP_ACK:
+      strlcpy(buf, "ACK", len);
+      break;
+    case PAP_NAK:
+      strlcpy(buf, "NAK", len);
+      break;
+    default:
+      snprintf(buf, len, "code%d", code);
+  }
+  return(buf);
+}
+
---
+++ tags/RELEASE_4_2rc2/src/ppp.h
@@ -0,0 +1,154 @@
+
+/*
+ * ppp.h
+ *
+ * Written by Archie Cobbs <archie@freebsd.org>
+ * Copyright (c) 1995-1999 Whistle Communications, Inc. All rights reserved.
+ * See ``COPYRIGHT.whistle''
+ */
+
+#ifndef _PPP_H_
+#define _PPP_H_
+
+/* Keep source files simple */
+
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <poll.h>
+#include <sys/time.h>
+#include <sys/uio.h>
+#include <sys/queue.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <string.h>
+#include <signal.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <errno.h>
+#include <err.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <machine/endian.h>
+#ifdef __DragonFly__
+#include <net/ppp_layer/ppp_defs.h>
+#else
+#include <net/ppp_defs.h>
+#endif
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <pthread.h>
+#include <pdel/structs/structs.h>
+#include <pdel/structs/type/array.h>
+#include <pdel/util/typed_mem.h>
+#include <pdel/util/pevent.h>
+#include <pdel/util/paction.h>
+#include <pdel/util/ghash.h>
+
+
+#include "defs.h"
+
+/*
+ * DEFINITIONS
+ */
+
+  /* Do our own version of assert() so it shows up in the logs */
+  #define assert(e)	((e) ? (void)0 : DoAssert(__FILE__, __LINE__, #e))
+
+  /* Giant Mutex handling */
+  #define GIANT_MUTEX_LOCK()	assert(pthread_mutex_lock(&gGiantMutex) == 0)
+  #define GIANT_MUTEX_UNLOCK()	assert(pthread_mutex_unlock(&gGiantMutex) == 0)
+
+  #define MUTEX_LOCK(m)		assert(pthread_mutex_lock(&m) == 0)
+  #define MUTEX_UNLOCK(m)	assert(pthread_mutex_unlock(&m) == 0)
+
+  #define RWLOCK_RDLOCK(m)	assert(pthread_rwlock_rdlock(&m) == 0)
+  #define RWLOCK_WRLOCK(m)	assert(pthread_rwlock_wrlock(&m) == 0)
+  #define RWLOCK_UNLOCK(m)	assert(pthread_rwlock_unlock(&m) == 0)
+  
+  #define SETOVERLOAD(q)	do {					\
+				    int t = (q);			\
+				    if (t > 60) {			\
+					gOverload = 100;		\
+				    } else if (t > 10) {		\
+					gOverload = (t - 10) * 2;	\
+				    } else {				\
+					gOverload = 0;			\
+				    }					\
+				} while (0)
+
+  #define OVERLOAD()		(gOverload > (random() % 100))
+
+  #define ADLG_WAN_AUTHORIZATION_FAILURE	0
+  #define ADLG_WAN_CONNECTED			1
+  #define ADLG_WAN_CONNECTING			2
+  #define ADLG_WAN_CONNECT_FAILURE		3
+  #define ADLG_WAN_DISABLED			4
+  #define ADLG_WAN_MESSAGE			5
+  #define ADLG_WAN_NEGOTIATION_FAILURE		6
+  #define ADLG_WAN_WAIT_FOR_DEMAND		7
+
+  /* internal 64 bit counters as workaround for the 32 bit 
+   * limitation for ng_ppp_link_stat
+   */
+  struct linkstats {
+	u_int64_t 	xmitFrames;	/* xmit frames on link */
+	u_int64_t 	xmitOctets;	/* xmit octets on link */
+	u_int64_t 	recvFrames;	/* recv frames on link */
+	u_int64_t	recvOctets;	/* recv octets on link */
+	u_int64_t 	badProtos;	/* frames rec'd with bogus protocol */
+	u_int64_t 	runts;		/* Too short MP fragments */
+	u_int64_t 	dupFragments;	/* MP frames with duplicate seq # */
+	u_int64_t	dropFragments;	/* MP fragments we had to drop */
+  };
+  typedef struct linkstat *LinkStats;
+
+#include "bund.h"
+#include "link.h"
+#include "rep.h"
+#include "phys.h"
+#include "msgdef.h"
+
+/*
+ * VARIABLES
+ */
+
+  extern PhysInfo	*gPhyses;		/* Physes */
+  extern Rep		*gReps;			/* Repeaters */
+  extern Bund		*gBundles;		/* Bundles */
+
+  extern int		gNumPhyses;		/* Total number of phys */
+  extern int		gNumReps;		/* Total number of repeaters */
+  extern int		gNumBundles;		/* Total number of bundles */
+  extern struct console	gConsole;
+  extern struct web	gWeb;
+  extern int		gBackground;
+  extern int		gShutdownInProgress;
+  extern int		gOverload;
+  extern pid_t		gPid;
+
+  extern struct globalconf	gGlobalConf;	/* Global config settings */
+
+  extern struct pevent_ctx	*gPeventCtx;
+  extern pthread_mutex_t	gGiantMutex;	/* Giant Mutex */
+
+  extern const char	*gVersion;		/* Program version string */
+  extern const char	*gConfigFile;		/* Main config file */
+  extern const char	*gConfDirectory;	/* Where the files are */
+
+/*
+ * FUNCTIONS
+ */
+
+  extern void		Greetings(void);
+  extern void		SendSignal(int sig);
+  extern void		DoExit(int code);
+  extern void		DoAssert(const char *file, int line, const char *x);
+
+#endif
+
