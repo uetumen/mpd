ab50a23650f184c2cb53377e8c41d916b210a54a
amotin <amotin@d3cc347e-4bbe-e711-9839-001b216d295b>
2007-12-07 19:12:37 +0000

    Fix multiple originated locally calls inside same tunnel simultaneous connect.

--- src/pptp_ctrl.c
+++ src/pptp_ctrl.c
@@ -1698,7 +1698,6 @@ PptpCtrlCheckConn(PptpCtrl c)
 	  PptpCtrlNewChanState(ch, PPTP_CHAN_ST_WAIT_OUT_REPLY);
 	  PptpCtrlWriteMsg(c, PPTP_OutCallRequest, &req);
 	}
-	return;
       }
       break;
     default:
