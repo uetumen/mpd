1f7b5655557d3dbd80e0b7d26fbab81499a54758
nobody <nobody@d3cc347e-4bbe-e711-9839-001b216d295b>
2008-01-06 19:39:43 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_4_4'.

---
+++ tags/RELEASE_4_4/src/ecp.c
@@ -0,0 +1,785 @@
+
+/*
+ * ecp.c
+ *
+ * Written by Archie Cobbs <archie@freebsd.org>
+ * Copyright (c) 1998-1999 Whistle Communications, Inc. All rights reserved.
+ * See ``COPYRIGHT.whistle''
+ */
+
+#include "ppp.h"
+#include "bund.h"
+#include "ecp.h"
+#include "fsm.h"
+#include "ngfunc.h"
+
+#include <netgraph/ng_message.h>
+#ifdef __DragonFly__
+#include <netgraph/socket/ng_socket.h>
+#else
+#include <netgraph/ng_socket.h>
+#endif
+#include <netgraph.h>
+
+/*
+ * DEFINITIONS
+ */
+
+  #define ECP_MAXFAILURE	7
+
+  #define ECP_KNOWN_CODES	(   (1 << CODE_CONFIGREQ)	\
+				  | (1 << CODE_CONFIGACK)	\
+				  | (1 << CODE_CONFIGNAK)	\
+				  | (1 << CODE_CONFIGREJ)	\
+				  | (1 << CODE_TERMREQ)		\
+				  | (1 << CODE_TERMACK)		\
+				  | (1 << CODE_CODEREJ)		\
+				  | (1 << CODE_RESETREQ)	\
+				  | (1 << CODE_RESETACK)	)
+
+  #define ECP_OVERHEAD		2
+
+  #define ECP_PEER_REJECTED(p,x)	((p)->peer_reject & (1<<(x)))
+  #define ECP_SELF_REJECTED(p,x)	((p)->self_reject & (1<<(x)))
+
+  #define ECP_PEER_REJ(p,x)	do{(p)->peer_reject |= (1<<(x));}while(0)
+  #define ECP_SELF_REJ(p,x)	do{(p)->self_reject |= (1<<(x));}while(0)
+
+/* Set menu options */
+
+  enum
+  {
+    SET_KEY,
+    SET_ACCEPT,
+    SET_DENY,
+    SET_ENABLE,
+    SET_DISABLE,
+    SET_YES,
+    SET_NO,
+  };
+
+/*
+ * INTERNAL FUNCTIONS
+ */
+
+  static void		EcpConfigure(Fsm fp);
+  static void		EcpUnConfigure(Fsm fp);
+  static u_char		*EcpBuildConfigReq(Fsm fp, u_char *cp);
+  static void		EcpDecodeConfig(Fsm fp, FsmOption a, int num, int mode);
+  static void		EcpLayerUp(Fsm fp);
+  static void		EcpLayerDown(Fsm fp);
+  static void		EcpFailure(Fsm f, enum fsmfail reason);
+  static void		EcpRecvResetReq(Fsm fp, int id, Mbuf bp);
+  static void		EcpRecvResetAck(Fsm fp, int id, Mbuf bp);
+
+  static int		EcpSetCommand(Context ctx, int ac, char *av[], void *arg);
+  static EncType	EcpFindType(int type, int *indexp);
+  static const char	*EcpTypeName(int type);
+
+/*
+ * GLOBAL VARIABLES
+ */
+
+  const struct cmdtab EcpSetCmds[] =
+  {
+    { "key string",			"Set encryption key",
+	EcpSetCommand, NULL, (void *) SET_KEY },
+    { "accept [opt ...]",		"Accept option",
+	EcpSetCommand, NULL, (void *) SET_ACCEPT },
+    { "deny [opt ...]",			"Deny option",
+	EcpSetCommand, NULL, (void *) SET_DENY },
+    { "enable [opt ...]",		"Enable option",
+	EcpSetCommand, NULL, (void *) SET_ENABLE },
+    { "disable [opt ...]",		"Disable option",
+	EcpSetCommand, NULL, (void *) SET_DISABLE },
+    { "yes [opt ...]",			"Enable and accept option",
+	EcpSetCommand, NULL, (void *) SET_YES },
+    { "no [opt ...]",			"Disable and deny option",
+	EcpSetCommand, NULL, (void *) SET_NO },
+    { NULL },
+  };
+
+/*
+ * INTERNAL VARIABLES
+ */
+
+/* These should be listed in order of preference */
+
+  static const EncType gEncTypes[] =
+  {
+#ifdef ENCRYPTION_DES
+    &gDeseBisEncType,
+    &gDeseEncType,
+#endif
+  };
+  #define ECP_NUM_PROTOS	(sizeof(gEncTypes) / sizeof(*gEncTypes))
+
+/* Corresponding option list */
+
+  static const struct confinfo *gConfList;
+
+/* Initializer for struct fsm fields */
+
+  static const struct fsmtype gEcpFsmType =
+  {
+    "ECP",
+    PROTO_ECP,
+    ECP_KNOWN_CODES,
+    LG_ECP, LG_ECP2,
+    FALSE,
+    NULL,
+    EcpLayerUp,
+    EcpLayerDown,
+    NULL,
+    NULL,
+    EcpBuildConfigReq,
+    EcpDecodeConfig,
+    EcpConfigure,
+    EcpUnConfigure,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    EcpFailure,
+    EcpRecvResetReq,
+    EcpRecvResetAck,
+  };
+
+/* Names for different types of encryption */
+
+  static const struct ecpname
+  {
+    u_char	type;
+    const char	*name;
+  }
+  gEcpTypeNames[] =
+  {
+    { ECP_TY_OUI,	"OUI" },
+    { ECP_TY_DESE,	"DESE" },
+    { ECP_TY_3DESE,	"3DESE" },
+    { ECP_TY_DESE_bis,	"DESE-bis" },
+    { 0,		NULL },
+  };
+
+/*
+ * EcpInit()
+ */
+
+void
+EcpInit(Bund b)
+{
+  EcpState	ecp = &b->ecp;
+
+/* Init ECP state for this bundle */
+
+  memset(ecp, 0, sizeof(*ecp));
+  FsmInit(&ecp->fsm, &gEcpFsmType, b);
+  ecp->fsm.conf.maxfailure = ECP_MAXFAILURE;
+
+/* Construct options list if we haven't done so already */
+
+  if (gConfList == NULL)
+  {
+    struct confinfo	*ci;
+    int			k;
+
+    ci = Malloc(MB_CRYPT, (ECP_NUM_PROTOS + 1) * sizeof(*ci));
+    for (k = 0; k < ECP_NUM_PROTOS; k++)
+    {
+      ci[k].option = k;
+      ci[k].peered = TRUE;
+      ci[k].name = gEncTypes[k]->name;
+    }
+    ci[k].name = NULL;
+    gConfList = (const struct confinfo *) ci;
+  }
+}
+
+/*
+ * EcpConfigure()
+ */
+
+static void
+EcpConfigure(Fsm fp)
+{
+    Bund 	b = (Bund)fp->arg;
+  EcpState	const ecp = &b->ecp;
+  int		k;
+
+  for (k = 0; k < ECP_NUM_PROTOS; k++)
+  {
+    EncType	const et = gEncTypes[k];
+
+    if (et->Configure)
+      (*et->Configure)(b);
+  }
+  ecp->xmit = NULL;
+  ecp->recv = NULL;
+  ecp->self_reject = 0;
+  ecp->peer_reject = 0;
+}
+
+/*
+ * EcpUnConfigure()
+ */
+
+static void
+EcpUnConfigure(Fsm fp)
+{
+    Bund 	b = (Bund)fp->arg;
+  EcpState	const ecp = &b->ecp;
+  int		k;
+
+  for (k = 0; k < ECP_NUM_PROTOS; k++)
+  {
+    EncType	const et = gEncTypes[k];
+
+    if (et->UnConfigure)
+      (*et->UnConfigure)(b);
+  }
+  ecp->xmit = NULL;
+  ecp->recv = NULL;
+  ecp->self_reject = 0;
+  ecp->peer_reject = 0;
+}
+
+/*
+ * EcpDataOutput()
+ *
+ * Encrypt a frame. Consumes the original packet.
+ */
+
+Mbuf
+EcpDataOutput(Bund b, Mbuf plain)
+{
+  EcpState	const ecp = &b->ecp;
+  Mbuf		cypher;
+
+  LogDumpBp(LG_ECP2, plain, "[%s] %s: xmit plain", Pref(&ecp->fsm), Fsm(&ecp->fsm));
+
+/* Encrypt packet */
+
+  if ((!ecp->xmit) || (!ecp->xmit->Encrypt))
+  {
+    Log(LG_ERR, ("[%s] %s: no encryption for xmit", Pref(&ecp->fsm), Fsm(&ecp->fsm)));
+    PFREE(plain);
+    return(NULL);
+  }
+  cypher = (*ecp->xmit->Encrypt)(b, plain);
+  LogDumpBp(LG_ECP2, cypher, "[%s] %s: xmit cypher", Pref(&ecp->fsm), Fsm(&ecp->fsm));
+
+/* Return result, with new protocol number */
+
+  return(cypher);
+}
+
+/*
+ * EcpDataInput()
+ *
+ * Decrypt incoming packet. If packet got garbled, return NULL.
+ * In any case, we consume the packet passed to us.
+ */
+
+Mbuf
+EcpDataInput(Bund b, Mbuf cypher)
+{
+  EcpState	const ecp = &b->ecp;
+  Mbuf		plain;
+
+  LogDumpBp(LG_ECP2, cypher, "[%s] %s: recv cypher", Pref(&ecp->fsm), Fsm(&ecp->fsm));
+
+/* Decrypt packet */
+
+  if ((!ecp->recv) || (!ecp->recv->Decrypt))
+  {
+    Log(LG_ERR, ("[%s] %s: no encryption for recv", Pref(&ecp->fsm), Fsm(&ecp->fsm)));
+    PFREE(cypher);
+    return(NULL);
+  }
+
+  plain = (*ecp->recv->Decrypt)(b, cypher);
+
+/* Decrypted ok? */
+
+  if (plain == NULL)
+  {
+    Log(LG_ECP, ("[%s] %s: decryption failed", Pref(&ecp->fsm), Fsm(&ecp->fsm)));
+    return(NULL);
+  }
+
+  LogDumpBp(LG_ECP2, plain, "[%s] %s: recv plain", Pref(&ecp->fsm), Fsm(&ecp->fsm));
+/* Done */
+
+  return(plain);
+}
+
+/*
+ * EcpUp()
+ */
+
+void
+EcpUp(Bund b)
+{
+  FsmUp(&b->ecp.fsm);
+}
+
+/*
+ * EcpDown()
+ */
+
+void
+EcpDown(Bund b)
+{
+  FsmDown(&b->ecp.fsm);
+}
+
+/*
+ * EcpOpen()
+ */
+
+void
+EcpOpen(Bund b)
+{
+  FsmOpen(&b->ecp.fsm);
+}
+
+/*
+ * EcpClose()
+ */
+
+void
+EcpClose(Bund b)
+{
+  FsmClose(&b->ecp.fsm);
+}
+
+/*
+ * EcpOpenCmd()
+ */
+
+void
+EcpOpenCmd(Context ctx)
+{
+  FsmOpen(&ctx->bund->ecp.fsm);
+}
+
+/*
+ * EcpCloseCmd()
+ */
+
+void
+EcpCloseCmd(Context ctx)
+{
+  FsmClose(&ctx->bund->ecp.fsm);
+}
+
+/*
+ * EcpFailure()
+ *
+ * This is fatal to the entire link if encryption is required.
+ */
+
+static void
+EcpFailure(Fsm fp, enum fsmfail reason)
+{
+    Bund 	b = (Bund)fp->arg;
+  EcpClose(b);
+  if (Enabled(&b->conf.options, BUND_CONF_CRYPT_REQD)) {
+    FsmFailure(&b->ipcp.fsm, FAIL_CANT_ENCRYPT);
+    FsmFailure(&b->ipv6cp.fsm, FAIL_CANT_ENCRYPT);
+  }
+}
+
+/*
+ * EcpStat()
+ */
+
+int
+EcpStat(Context ctx, int ac, char *av[], void *arg)
+{
+  EcpState	const ecp = &ctx->bund->ecp;
+
+  Printf("[%s] %s [%s]\r\n", Pref(&ecp->fsm), Fsm(&ecp->fsm), FsmStateName(ecp->fsm.state));
+  Printf("Enabled protocols:\r\n");
+  OptStat(ctx, &ecp->options, gConfList);
+  Printf("Outgoing encryption:\r\n");
+  Printf("\tProto\t: %s\r\n", ecp->xmit ? ecp->xmit->name : "none");
+  if (ecp->xmit && ecp->xmit->Stat)
+    ecp->xmit->Stat(ctx, ECP_DIR_XMIT);
+  Printf("\tResets\t: %d\r\n", ecp->xmit_resets);
+  Printf("Incoming decryption:\r\n");
+  Printf("\tProto\t: %s\r\n", ecp->recv ? ecp->recv->name : "none");
+  if (ecp->recv && ecp->recv->Stat)
+    ecp->recv->Stat(ctx, ECP_DIR_RECV);
+  Printf("\tResets\t: %d\r\n", ecp->recv_resets);
+  return(0);
+}
+
+/*
+ * EcpSendResetReq()
+ */
+
+void
+EcpSendResetReq(Fsm fp)
+{
+    Bund 	b = (Bund)fp->arg;
+  EcpState	const ecp = &b->ecp;
+  EncType	const et = ecp->recv;
+  Mbuf		bp = NULL;
+
+  assert(et);
+  ecp->recv_resets++;
+  if (et->SendResetReq)
+    bp = (*et->SendResetReq)(b);
+  Log(LG_ECP, ("[%s] %s: SendResetReq", Pref(fp), Fsm(fp)));
+  FsmOutputMbuf(fp, CODE_RESETREQ, fp->reqid++, bp);
+}
+
+/*
+ * EcpRecvResetReq()
+ */
+
+void
+EcpRecvResetReq(Fsm fp, int id, Mbuf bp)
+{
+    Bund 	b = (Bund)fp->arg;
+  EcpState	const ecp = &b->ecp;
+  EncType	const et = ecp->xmit;
+
+  ecp->xmit_resets++;
+  bp = (et && et->RecvResetReq) ? (*et->RecvResetReq)(b, id, bp) : NULL;
+  Log(fp->log, ("[%s] %s: SendResetAck", Pref(fp), Fsm(fp)));
+  FsmOutputMbuf(fp, CODE_RESETACK, id, bp);
+}
+
+/*
+ * EcpRecvResetAck()
+ */
+
+static void
+EcpRecvResetAck(Fsm fp, int id, Mbuf bp)
+{
+    Bund 	b = (Bund)fp->arg;
+  EcpState	const ecp = &b->ecp;
+  EncType	const et = ecp->recv;
+
+  if (et && et->RecvResetAck)
+    (*et->RecvResetAck)(b, id, bp);
+}
+
+/*
+ * EcpInput()
+ */
+
+void
+EcpInput(Bund b, Mbuf bp)
+{
+  FsmInput(&b->ecp.fsm, bp);
+}
+
+/*
+ * EcpBuildConfigReq()
+ */
+
+static u_char *
+EcpBuildConfigReq(Fsm fp, u_char *cp)
+{
+    Bund 	b = (Bund)fp->arg;
+  EcpState	const ecp = &b->ecp;
+  int		type;
+
+/* Put in all options that peer hasn't rejected */
+
+  for (ecp->xmit = NULL, type = 0; type < ECP_NUM_PROTOS; type++)
+  {
+    EncType	const et = gEncTypes[type];
+
+    if (Enabled(&ecp->options, type) && !ECP_PEER_REJECTED(ecp, type))
+    {
+      cp = (*et->BuildConfigReq)(b, cp);
+      if (!ecp->xmit)
+	ecp->xmit = et;
+    }
+  }
+  return(cp);
+}
+
+/*
+ * EcpLayerUp()
+ *
+ * Called when ECP has reached the OPENED state
+ */
+
+static void
+EcpLayerUp(Fsm fp)
+{
+    Bund 	b = (Bund)fp->arg;
+  EcpState	const ecp = &b->ecp;
+  struct ngm_connect    cn;
+
+  /* Initialize */
+  if (ecp->xmit && ecp->xmit->Init)
+    (*ecp->xmit->Init)(b, ECP_DIR_XMIT);
+  if (ecp->recv && ecp->recv->Init)
+    (*ecp->recv->Init)(b, ECP_DIR_RECV);
+
+  if (ecp->recv && ecp->recv->Decrypt) 
+  {
+    /* Connect a hook from the bpf node to our socket node */
+    snprintf(cn.path, sizeof(cn.path), "%s", MPD_HOOK_PPP);
+    snprintf(cn.ourhook, sizeof(cn.ourhook), "%s", NG_PPP_HOOK_DECRYPT);
+    snprintf(cn.peerhook, sizeof(cn.peerhook), "%s", NG_PPP_HOOK_DECRYPT);
+    if (NgSendMsg(b->csock, ".",
+	    NGM_GENERIC_COOKIE, NGM_CONNECT, &cn, sizeof(cn)) < 0) {
+	Log(LG_ERR, ("[%s] can't connect \"%s\"->\"%s\" and \"%s\"->\"%s\": %s",
+        b->name, ".", cn.ourhook, cn.path, cn.peerhook,  strerror(errno)));
+    }
+  }
+  if (ecp->xmit && ecp->xmit->Encrypt)
+  {
+    /* Connect a hook from the bpf node to our socket node */
+    snprintf(cn.path, sizeof(cn.path), "%s", MPD_HOOK_PPP);
+    snprintf(cn.ourhook, sizeof(cn.ourhook), "%s", NG_PPP_HOOK_ENCRYPT);
+    snprintf(cn.peerhook, sizeof(cn.peerhook), "%s", NG_PPP_HOOK_ENCRYPT);
+    if (NgSendMsg(b->csock, ".",
+	    NGM_GENERIC_COOKIE, NGM_CONNECT, &cn, sizeof(cn)) < 0) {
+	Log(LG_ERR, ("[%s] can't connect \"%s\"->\"%s\" and \"%s\"->\"%s\": %s",
+        b->name, ".", cn.ourhook, cn.path, cn.peerhook, strerror(errno)));
+    }
+  }
+
+  Log(LG_ECP, ("  Encrypt using: %s", !ecp->xmit ? "none" : ecp->xmit->name));
+  Log(LG_ECP, ("  Decrypt using: %s", !ecp->recv ? "none" : ecp->recv->name));
+
+  /* Update PPP node config */
+  b->pppConfig.bund.enableEncryption = (ecp->xmit != NULL);
+  b->pppConfig.bund.enableDecryption = (ecp->recv != NULL);
+  NgFuncSetConfig(b);
+
+  /* Update interface MTU */
+  BundUpdateParams(b);
+}
+
+/*
+ * EcpLayerDown()
+ *
+ * Called when ECP leaves the OPENED state
+ */
+
+static void
+EcpLayerDown(Fsm fp)
+{
+    Bund 	b = (Bund)fp->arg;
+  EcpState	const ecp = &b->ecp;
+
+  /* Update PPP node config */
+  b->pppConfig.bund.enableEncryption = 0;
+  b->pppConfig.bund.enableDecryption = 0;
+  NgFuncSetConfig(b);
+
+  /* Update interface MTU */
+  BundUpdateParams(b);
+
+  if (ecp->xmit != NULL && ecp->xmit->Encrypt != NULL) {
+    /* Disconnect hook. */
+    if (NgFuncDisconnect(b->csock, b->name, ".:", NG_PPP_HOOK_ENCRYPT) < 0) {
+	Log(LG_ERR, ("can't remove hook %s: %s", NG_PPP_HOOK_ENCRYPT, strerror(errno)));
+    }
+  }
+  
+  if (ecp->recv != NULL && ecp->recv->Decrypt != NULL) {
+    /* Disconnect hook. */
+    if (NgFuncDisconnect(b->csock, b->name, ".:", NG_PPP_HOOK_DECRYPT) < 0) {
+	Log(LG_ERR, ("can't remove hook %s: %s", NG_PPP_HOOK_DECRYPT, strerror(errno)));
+    }
+  }
+
+  if (ecp->xmit && ecp->xmit->Cleanup)
+    (ecp->xmit->Cleanup)(b, ECP_DIR_XMIT);
+  if (ecp->recv && ecp->recv->Cleanup)
+    (ecp->recv->Cleanup)(b, ECP_DIR_RECV);
+    
+  ecp->xmit_resets = 0;
+  ecp->recv_resets = 0;
+}
+
+/*
+ * EcpDecodeConfig()
+ */
+
+static void
+EcpDecodeConfig(Fsm fp, FsmOption list, int num, int mode)
+{
+    Bund 	b = (Bund)fp->arg;
+  EcpState	const ecp = &b->ecp;
+  u_int		ackSizeSave, rejSizeSave;
+  int		k, rej;
+
+  /* Forget our previous choice on new request */
+  if (mode == MODE_REQ)
+    ecp->recv = NULL;
+
+/* Decode each config option */
+
+  for (k = 0; k < num; k++)
+  {
+    FsmOption	const opt = &list[k];
+    int		index;
+    EncType	et;
+
+    Log(LG_ECP, (" %s", EcpTypeName(opt->type)));
+    if ((et = EcpFindType(opt->type, &index)) == NULL)
+    {
+      if (mode == MODE_REQ)
+      {
+	Log(LG_ECP, ("   Not supported"));
+	FsmRej(fp, opt);
+      }
+      continue;
+    }
+    switch (mode)
+    {
+      case MODE_REQ:
+	ackSizeSave = gAckSize;
+	rejSizeSave = gRejSize;
+	rej = (!Acceptable(&ecp->options, index)
+	  || ECP_SELF_REJECTED(ecp, index)
+	  || (ecp->recv && ecp->recv != et));
+	if (rej)
+	{
+	  (*et->DecodeConfig)(fp, opt, MODE_NOP);
+	  FsmRej(fp, opt);
+	  break;
+	}
+	(*et->DecodeConfig)(fp, opt, mode);
+	if (gRejSize != rejSizeSave)		/* we rejected it */
+	{
+	  ECP_SELF_REJ(ecp, index);
+	  break;
+	}
+	if (gAckSize != ackSizeSave)		/* we accepted it */
+	  ecp->recv = et;
+	break;
+
+      case MODE_NAK:
+	(*et->DecodeConfig)(fp, opt, mode);
+	break;
+
+      case MODE_REJ:
+	(*et->DecodeConfig)(fp, opt, mode);
+	ECP_PEER_REJ(ecp, index);
+	break;
+
+      case MODE_NOP:
+	(*et->DecodeConfig)(fp, opt, mode);
+	break;
+    }
+  }
+}
+
+/*
+ * EcpSubtractBloat()
+ *
+ * Given that "size" is our MTU, return the maximum length frame
+ * we can encrypt without the result being longer than "size".
+ */
+
+int
+EcpSubtractBloat(Bund b, int size)
+{
+  EcpState	const ecp = &b->ecp;
+
+  /* Check transmit encryption */
+  if (OPEN_STATE(ecp->fsm.state) && ecp->xmit && ecp->xmit->SubtractBloat)
+    size = (*ecp->xmit->SubtractBloat)(b, size);
+
+  /* Account for ECP's protocol number overhead */
+  if (OPEN_STATE(ecp->fsm.state))
+    size -= ECP_OVERHEAD;
+
+  /* Done */
+  return(size);
+}
+
+/*
+ * EcpSetCommand()
+ */
+
+static int
+EcpSetCommand(Context ctx, int ac, char *av[], void *arg)
+{
+  EcpState	const ecp = &ctx->bund->ecp;
+
+  if (ac == 0)
+    return(-1);
+  switch ((intptr_t)arg)
+  {
+    case SET_KEY:
+      if (ac != 1)
+	return(-1);
+      snprintf(ecp->key, sizeof(ecp->key), "%s", av[0]);
+      break;
+
+    case SET_ACCEPT:
+      AcceptCommand(ac, av, &ecp->options, gConfList);
+      break;
+
+    case SET_DENY:
+      DenyCommand(ac, av, &ecp->options, gConfList);
+      break;
+
+    case SET_ENABLE:
+      EnableCommand(ac, av, &ecp->options, gConfList);
+      break;
+
+    case SET_DISABLE:
+      DisableCommand(ac, av, &ecp->options, gConfList);
+      break;
+
+    case SET_YES:
+      YesCommand(ac, av, &ecp->options, gConfList);
+      break;
+
+    case SET_NO:
+      NoCommand(ac, av, &ecp->options, gConfList);
+      break;
+
+    default:
+      assert(0);
+  }
+  return(0);
+}
+
+/*
+ * EcpFindType()
+ */
+
+static EncType
+EcpFindType(int type, int *indexp)
+{
+  int	k;
+
+  for (k = 0; k < ECP_NUM_PROTOS; k++)
+    if (gEncTypes[k]->type == type)
+    {
+      if (indexp)
+	*indexp = k;
+      return(gEncTypes[k]);
+    }
+  return(NULL);
+}
+
+/*
+ * EcpTypeName()
+ */
+
+static const char *
+EcpTypeName(int type)
+{
+  const struct ecpname	*p;
+
+  for (p = gEcpTypeNames; p->name; p++)
+    if (p->type == type)
+      return(p->name);
+  return("UNKNOWN");
+}
---
+++ tags/RELEASE_4_4/src/rep.c
@@ -0,0 +1,523 @@
+
+/*
+ * rep.c
+ *
+ * Written by Alexander Motin <mav@FreeBSD.org>
+ */
+
+#include "ppp.h"
+#include "rep.h"
+#include "msg.h"
+#include "ngfunc.h"
+#include "log.h"
+#include "util.h"
+
+#include <netgraph/ng_message.h>
+#ifdef __DragonFly__
+#include <netgraph/socket/ng_socket.h>
+#include <netgraph/tee/ng_tee.h>
+#else
+#include <netgraph/ng_socket.h>
+#include <netgraph/ng_tee.h>
+#endif
+#include <netgraph.h>
+
+/*
+ * DEFINITIONS
+ */
+
+  /* Set menu options */
+  enum {
+    SET_ACCEPT,
+    SET_DENY,
+    SET_ENABLE,
+    SET_DISABLE,
+    SET_YES,
+    SET_NO,
+  };
+
+/*
+ * INTERNAL FUNCTIONS
+ */
+
+  static Rep	RepFind(char *name);
+  static int	RepSetCommand(Context ctx, int ac, char *av[], void *arg);
+  static void	RepShowLinks(Context ctx, Rep r);
+
+/*
+ * GLOBAL VARIABLES
+ */
+
+  struct discrim	self_discrim;
+
+  const struct cmdtab RepSetCmds[] = {
+    { "accept [opt ...]",		"Accept option",
+	RepSetCommand, NULL, (void *) SET_ACCEPT },
+    { "deny [opt ...]",			"Deny option",
+	RepSetCommand, NULL, (void *) SET_DENY },
+    { "enable [opt ...]",		"Enable option",
+	RepSetCommand, NULL, (void *) SET_ENABLE },
+    { "disable [opt ...]",		"Disable option",
+	RepSetCommand, NULL, (void *) SET_DISABLE },
+    { "yes [opt ...]",			"Enable and accept option",
+	RepSetCommand, NULL, (void *) SET_YES },
+    { "no [opt ...]",			"Disable and deny option",
+	RepSetCommand, NULL, (void *) SET_NO },
+    { NULL },
+  };
+
+/*
+ * INTERNAL VARIABLES
+ */
+
+  static const struct confinfo	gConfList[] = {
+    { 0,	0,			NULL		},
+  };
+
+/*
+ * RepOpen()
+ */
+
+void
+RepOpen(void)
+{
+//  MsgSend(rep->msgs, MSG_OPEN, NULL);
+}
+
+/*
+ * RepClose()
+ */
+
+void
+RepClose(void)
+{
+//  MsgSend(rep->msgs, MSG_CLOSE, NULL);
+}
+
+/*
+ * RepIncoming()
+ */
+
+void
+RepIncoming(PhysInfo p)
+{
+    Rep		r = p->rep;
+    int		n = (r->physes[0] == p)?0:1;
+    struct ngm_mkpeer       mkp;
+    char	buf[64];
+    
+    r->initiator = n;
+
+    Log(LG_REP, ("[%s] REP: INCOMING event from %s (%d)",
+	r->name, p->name, n));
+
+    if (r->csock <= 0) {
+	/* Create a new netgraph node to control TCP ksocket node. */
+	if (NgMkSockNode(NULL, &r->csock, NULL) < 0) {
+    	    Log(LG_ERR, ("[%s] REP: can't create control socket: %s",
+    		r->name, strerror(errno)));
+    	    PhysClose(p);
+	    return;
+	}
+	(void)fcntl(r->csock, F_SETFD, 1);
+    }
+
+    snprintf(mkp.type, sizeof(mkp.type), "%s", NG_TEE_NODE_TYPE);
+    snprintf(mkp.ourhook, sizeof(mkp.ourhook), "tee");
+    snprintf(mkp.peerhook, sizeof(mkp.peerhook), NG_TEE_HOOK_LEFT2RIGHT);
+    if (NgSendMsg(r->csock, ".:", NGM_GENERIC_COOKIE,
+        NGM_MKPEER, &mkp, sizeof(mkp)) < 0) {
+    	Log(LG_ERR, ("[%s] REP: can't attach %s %s node: %s",
+    	    p->name, NG_TEE_NODE_TYPE, mkp.ourhook, strerror(errno)));
+	close(r->csock);
+    	PhysClose(p);
+	return;
+    }
+
+    /* Get tee node ID */
+    if ((r->node_id = NgGetNodeID(r->csock, ".:tee")) == 0) {
+	Log(LG_ERR, ("[%s] Rep: Cannot get %s node id: %s",
+	    p->name, NG_TEE_NODE_TYPE, strerror(errno)));
+	close(r->csock);
+    	PhysClose(p);
+	return;
+    };
+    
+    PhysGetCallingNum(r->physes[n], buf, sizeof(buf));
+    PhysSetCallingNum(r->physes[1-n], buf);
+
+    PhysGetCalledNum(r->physes[n], buf, sizeof(buf));
+    PhysSetCalledNum(r->physes[1-n], buf);
+
+    PhysOpen(r->physes[1-n]);
+}
+
+/*
+ * RepUp()
+ */
+
+void
+RepUp(PhysInfo p)
+{
+    Rep r = p->rep;
+    int n = (r->physes[0] == p)?0:1;
+    
+    Log(LG_REP, ("[%s] REP: UP event from %s (%d)",
+	r->name, p->name, n));
+
+    r->p_up |= (1 << n);
+    
+    if (n != r->initiator) {
+	PhysOpen(r->physes[1-n]);
+    }
+
+    if (r->p_up == 3 && r->csock > 0 && r->node_id) {
+	char path[NG_PATHLEN + 1];
+	
+	snprintf(path, sizeof(path), "[%x]:", r->node_id);
+	NgFuncShutdownNode(r->csock, r->name, path);
+	r->node_id = 0;
+	close(r->csock);
+	r->csock = -1;
+    }
+}
+
+/*
+ * RepDown()
+ */
+
+void
+RepDown(PhysInfo p)
+{
+    Rep r = p->rep;
+    int n = (r->physes[0] == p)?0:1;
+
+    Log(LG_REP, ("[%s] REP: DOWN event from %s (%d)",
+	r->name, p->name, n));
+
+    r->p_up &= ~(1 << n);
+    
+    PhysClose(r->physes[0]);
+    PhysClose(r->physes[1]);
+
+    if (r->csock > 0 && r->node_id) {
+	char path[NG_PATHLEN + 1];
+	
+	snprintf(path, sizeof(path), "[%x]:", r->node_id);
+	NgFuncShutdownNode(r->csock, r->name, path);
+	r->node_id = 0;
+	close(r->csock);
+	r->csock = -1;
+    }
+}
+
+/*
+ * RepIsSync()
+ */
+
+int
+RepIsSync(PhysInfo p) {
+    Rep r = p->rep;
+    int n = (r->physes[0] == p)?0:1;
+    
+    return (PhysIsSync(r->physes[1-n]));
+}
+
+/*
+ * RepSetAccm()
+ */
+
+void
+RepSetAccm(PhysInfo p, u_int32_t xmit, u_int32_t recv) {
+    Rep r = p->rep;
+    int n = (r->physes[0] == p)?0:1;
+    
+    Log(LG_REP, ("[%s] REP: SetAccm(0x%08x, 0x%08x) from %s (%d)",
+	r->name, xmit, recv, p->name, n));
+
+    PhysSetAccm(r->physes[1-n], xmit, recv);
+}
+
+/*
+ * RepGetHook()
+ */
+
+int
+RepGetHook(PhysInfo p, char *path, char *hook)
+{
+    Rep r = p->rep;
+    int n = (r->physes[0] == p)?0:1;
+
+    if (r->node_id == 0)
+	return (0);
+
+    snprintf(path, NG_PATHLEN, "[%lx]:", (u_long)r->node_id);
+    if (n == 0)
+	snprintf(hook, NG_HOOKLEN, NG_TEE_HOOK_LEFT);
+    else
+	snprintf(hook, NG_HOOKLEN, NG_TEE_HOOK_RIGHT);
+    return (1);
+}
+
+/*
+ * RepCommand()
+ *
+ * Show list of all bundles or set bundle
+ */
+
+int
+RepCommand(Context ctx, int ac, char *av[], void *arg)
+{
+  Rep	r;
+  int	k;
+
+  switch (ac) {
+    case 0:
+
+      Printf("Defined repeaters:\r\n");
+
+      for (k = 0; k < gNumReps; k++)
+	if ((r = gReps[k]) != NULL) {
+	  Printf("\t%-15s", r->name);
+	  RepShowLinks(ctx, r);
+	}
+      break;
+
+    case 1:
+
+      /* Change bundle, and link also if needed */
+      if ((r = RepFind(av[0])) != NULL) {
+	ctx->rep = r;
+	ctx->phys = r->physes[0];
+	ctx->bund = NULL;
+	ctx->lnk = NULL;
+      } else
+	Printf("Repeater \"%s\" not defined.\r\n", av[0]);
+      break;
+
+    default:
+      return(-1);
+  }
+  return(0);
+}
+
+/*
+ * RepCreateCmd()
+ *
+ * Create a new repeater.
+ */
+
+int
+RepCreateCmd(Context ctx, int ac, char *av[], void *arg)
+{
+    Rep		r;
+    PhysInfo	new_link;
+    int		k;
+
+  /* Args */
+  if (ac != 3)
+    return(-1);
+
+#if NG_NODESIZ>=32
+  if (strlen(av[0])>16) {
+#else
+  if (strlen(av[0])>6) {
+#endif
+    Log(LG_ERR, ("repeater name \"%s\" is too long", av[0]));
+    return (-1);
+  }
+
+  /* See if repeater name already taken */
+  if ((r = RepFind(av[0])) != NULL) {
+    Log(LG_ERR, ("repeater \"%s\" already exists", av[0]));
+    return (-1);
+  }
+
+  /* Create a new repeater structure */
+  r = Malloc(MB_REP, sizeof(*r));
+  snprintf(r->name, sizeof(r->name), "%s", av[0]);
+  r->csock = -1;
+
+  /* Create each link and add it to the repeater */
+  for (k = 1; k < ac; k++) {
+#if NG_NODESIZ>=32
+    if (strlen(av[k])>16) {
+#else
+    if (strlen(av[k])>6) {
+#endif
+	Log(LG_ERR, ("phys name \"%s\" is too long", av[k]));
+        RepShutdown(r);
+	return (-1);
+    }
+    if ((new_link = PhysInit(av[k], NULL, r)) == NULL) {
+      Log(LG_ERR, ("[%s] creation of phys \"%s\" failed", av[0], av[k]));
+      RepShutdown(r);
+      return (-1);
+    } else {
+      r->physes[k - 1] = new_link;
+    }
+  }
+
+  /* Add repeater to the list of repeaters and make it the current active repeater */
+  for (k = 0; k < gNumReps && gReps[k] != NULL; k++);
+  if (k == gNumReps)			/* add a new repeater pointer */
+    LengthenArray(&gReps, sizeof(*gReps), &gNumReps, MB_REP);
+  gReps[k] = r;
+  
+  ctx->rep = r;
+  ctx->phys = r->physes[0];
+  ctx->lnk = NULL;
+  ctx->bund = NULL;
+
+  /* Done */
+  return(0);
+}
+
+/*
+ * RepShutdown()
+ *
+ */
+ 
+void
+RepShutdown(Rep r)
+{
+    int		k;
+    
+    for(k = 0; k < 2; k++) {
+	if (r->physes[k])
+	    PhysShutdown(r->physes[k]);
+    }
+
+    for (k = 0; 
+	k < gNumReps && gReps[k] != r;
+	k++);
+    if (k < gNumReps)
+	gReps[k] = NULL;
+
+    if (r->csock > 0 && r->node_id) {
+	char path[NG_PATHLEN + 1];
+	
+	snprintf(path, sizeof(path), "[%x]:", r->node_id);
+	NgFuncShutdownNode(r->csock, r->name, path);
+	r->node_id = 0;
+	close(r->csock);
+	r->csock = -1;
+    }
+    
+    Freee(MB_REP, r);
+}
+
+/*
+ * RepStat()
+ *
+ * Show state of a repeater
+ */
+
+int
+RepStat(Context ctx, int ac, char *av[], void *arg)
+{
+  Rep	r;
+
+  /* Find repeater they're talking about */
+  switch (ac) {
+    case 0:
+      r = ctx->rep;
+      break;
+    case 1:
+      if ((r = RepFind(av[0])) == NULL) {
+	Printf("Repeater \"%s\" not defined.\r\n", av[0]);
+	return(0);
+      }
+      break;
+    default:
+      return(-1);
+  }
+
+  /* Show stuff about the repeater */
+  Printf("Repeater %s:\r\n", r->name);
+  Printf("\tPhyses          : ");
+  RepShowLinks(ctx, r);
+
+  return(0);
+}
+
+/*
+ * RepShowLinks()
+ */
+
+static void
+RepShowLinks(Context ctx, Rep r)
+{
+    int		j;
+
+  for (j = 0; j < 2; j++) {
+    Printf("%s", r->physes[j]->name);
+    if (!r->physes[j]->type)
+      Printf("[no type/%s] ",
+      	gPhysStateNames[r->physes[j]->state]);
+    else
+      Printf("[%s/%s] ", r->physes[j]->type->name,
+      	gPhysStateNames[r->physes[j]->state]);
+  }
+  Printf("\r\n");
+}
+
+/*
+ * RepFind()
+ *
+ * Find a repeater structure
+ */
+
+static Rep
+RepFind(char *name)
+{
+  int	k;
+
+  for (k = 0;
+    k < gNumReps && (strcmp(gReps[k]->name, name));
+    k++);
+  return((k < gNumReps) ? gReps[k] : NULL);
+}
+
+/*
+ * RepSetCommand()
+ */
+
+static int
+RepSetCommand(Context ctx, int ac, char *av[], void *arg)
+{
+    Rep		r = ctx->rep;
+
+  if (ac == 0)
+    return(-1);
+  switch ((intptr_t)arg) {
+    case SET_ACCEPT:
+      AcceptCommand(ac, av, &r->options, gConfList);
+      break;
+
+    case SET_DENY:
+      DenyCommand(ac, av, &r->options, gConfList);
+      break;
+
+    case SET_ENABLE:
+      EnableCommand(ac, av, &r->options, gConfList);
+      break;
+
+    case SET_DISABLE:
+      DisableCommand(ac, av, &r->options, gConfList);
+      break;
+
+    case SET_YES:
+      YesCommand(ac, av, &r->options, gConfList);
+      break;
+
+    case SET_NO:
+      NoCommand(ac, av, &r->options, gConfList);
+      break;
+
+    default:
+      assert(0);
+  }
+  return(0);
+}
+
