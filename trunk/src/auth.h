04709022ba9fb9813522c97371d5f0e6243fd4c9
dadv <dadv@d3cc347e-4bbe-e711-9839-001b216d295b>
2019-01-02 06:48:07 +0000

    auth.h: remove unused struct radiusconf radius
    Found by -Wmissing-variable-declarations.

--- trunk/src/auth.h
+++ trunk/src/auth.h
@@ -240,8 +240,6 @@ struct auth {
 };
 typedef struct auth *Auth;
 
-struct radiusconf radius;		/* RADIUS configuration */
-
  /*
   * Interface between the auth-backend (secret file, RADIUS, etc.) and Mpd's
   * internal structs.
