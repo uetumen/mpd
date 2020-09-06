bd268ad2cabcda907a5f85e70bc99386e7fde23c
dadv <dadv@d3cc347e-4bbe-e711-9839-001b216d295b>
2020-09-06 04:40:58 +0000

    Merge net/mpd5/patch-src_pppoe.c from FreeBSD Ports collection: fix processing interface name containing dots and add basic parsing of PADM messages.
    M    pppoe.c

--- trunk/src/pppoe.c
+++ trunk/src/pppoe.c
@@ -72,6 +72,7 @@
 
 /* Per link private info */
 struct pppoeinfo {
+	char		iface[IFNAMSIZ];	/* PPPoE interface name */
 	char		path[MAX_PATH];		/* PPPoE node path */
 	char		hook[NG_HOOKSIZ];	/* hook on that node */
 	char		session[MAX_SESSION];	/* session name */
@@ -166,7 +167,7 @@ static int 	PppoeListen(Link l);
 static int 	PppoeUnListen(Link l);
 static void	PppoeNodeUpdate(Link l);
 static void	PppoeListenEvent(int type, void *arg);
-static int 	CreatePppoeNode(struct PppoeIf *PIf, const char *path, const char *hook);
+static int 	CreatePppoeNode(struct PppoeIf *PIf, const char *iface, const char *path, const char *hook);
 
 static void	PppoeDoClose(Link l);
 
@@ -282,6 +283,7 @@ PppoeInit(Link l)
 	pe = (PppoeInfo)(l->info = Malloc(MB_PHYS, sizeof(*pe)));
 	pe->incoming = 0;
 	pe->opened = 0;
+	snprintf(pe->iface, sizeof(pe->iface), "undefined");
 	snprintf(pe->path, sizeof(pe->path), "undefined:");
 	snprintf(pe->hook, sizeof(pe->hook), "undefined");
 	snprintf(pe->session, sizeof(pe->session), "*");
@@ -661,6 +663,16 @@ PppoeCtrlReadEvent(int type, void *arg)
 		break;
 	    }
 #endif
+#ifdef NGM_PPPOE_PADM_COOKIE
+	    case NGM_PPPOE_HURL:
+		Log(LG_PHYS, ("PPPoE: rec'd HURL \"%s\"",
+		  ((struct ngpppoe_padm *)u.resp.data)->msg));
+		break;
+	    case NGM_PPPOE_MOTM:
+		Log(LG_PHYS, ("PPPoE: rec'd MOTM \"%s\"",
+		  ((struct ngpppoe_padm *)u.resp.data)->msg));
+		break;
+#endif
 	    default:
 		Log(LG_PHYS, ("PPPoE: rec'd command %lu from \"%s\"",
 		    (u_long)u.resp.header.cmd, path));
@@ -696,6 +708,7 @@ PppoeStat(Context ctx)
 	}
 
 	Printf("PPPoE configuration:\r\n");
+	Printf("\tIface Name   : %s\r\n", pe->iface);
 	Printf("\tIface Node   : %s\r\n", pe->path);
 	Printf("\tIface Hook   : %s\r\n", pe->hook);
 	Printf("\tSession      : %s\r\n", pe->session);
@@ -752,12 +765,8 @@ static int
 PppoePeerIface(Link l, void *buf, size_t buf_len)
 {
 	PppoeInfo	const pppoe = (PppoeInfo)l->info;
-	char iface[IFNAMSIZ];
 
-	strlcpy(iface, pppoe->path, sizeof(iface));
-	if (iface[strlen(iface) - 1] == ':')
-		iface[strlen(iface) - 1] = '\0';
-	strlcpy(buf, iface, buf_len);
+	strlcpy(buf, pppoe->iface, buf_len);
 	return (0);
 }
 
@@ -884,7 +893,7 @@ PppoeGetMru(Link l, int conf)
 }
 
 static int 
-CreatePppoeNode(struct PppoeIf *PIf, const char *path, const char *hook)
+CreatePppoeNode(struct PppoeIf *PIf, const char *iface, const char *path, const char *hook)
 {
         union {
 		u_char          buf[sizeof(struct ng_mesg) + 2048];
@@ -896,11 +905,6 @@ CreatePppoeNode(struct PppoeIf *PIf, const char *path, const char *hook)
 	uint32_t f;
 
 	/* Make sure interface is up. */
-	char iface[IFNAMSIZ];
-
-	strlcpy(iface, path, sizeof(iface));
-	if (iface[strlen(iface) - 1] == ':')
-		iface[strlen(iface) - 1] = '\0';
 	if (IfaceSetFlag(iface, IFF_UP) != 0) {
 		Perror("[%s] PPPoE: can't bring up interface", iface);
 		return (0);
@@ -1505,7 +1509,7 @@ PppoeGetNode(Link l)
 		    l->name));
 		return;
 	}
-	if (CreatePppoeNode(&PppoeIfs[free], pi->path, pi->hook)) {
+	if (CreatePppoeNode(&PppoeIfs[free], pi->iface, pi->path, pi->hook)) {
 		strlcpy(PppoeIfs[free].ifnodepath,
 		    pi->path,
 		    sizeof(PppoeIfs[free].ifnodepath));
@@ -1677,7 +1681,7 @@ PppoeSetCommand(Context ctx, int ac, const char *const av[], const void *arg)
 {
 	const PppoeInfo pi = (PppoeInfo) ctx->lnk->info;
 	const char *hookname = ETHER_DEFAULT_HOOK;
-	const char *colon;
+	int i;
 #ifdef NGM_PPPOE_SETMAXP_COOKIE
 	int ap;
 #endif
@@ -1688,9 +1692,17 @@ PppoeSetCommand(Context ctx, int ac, const char *const av[], const void *arg)
 			hookname = av[1];
 			/* fall through */
 		case 1:
-			colon = (av[0][strlen(av[0]) - 1] == ':') ? "" : ":";
-			snprintf(pi->path, sizeof(pi->path),
-			    "%s%s", av[0], colon);
+			strlcpy(pi->iface, av[0], sizeof(pi->iface));
+			strlcpy(pi->path, pi->iface, sizeof(pi->path) - 1);
+			for (i = 0; i < sizeof(pi->path) - 1; i++) {
+				if (pi->path[i] == '.' || pi->path[i] == ':')
+					pi->path[i] = '_';
+				else if (pi->path[i] == '\0') {
+					pi->path[i] = ':';
+					pi->path[i + 1] = '\0';
+					break;
+				}
+			}
 			strlcpy(pi->hook, hookname, sizeof(pi->hook));
 			break;
 		default:
